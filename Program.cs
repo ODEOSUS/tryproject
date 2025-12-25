26eda19773203457215db4d0e4ee6a73156f99b4
ODEOSUS <aostra11235813@gmail.com>
2025-12-26 02:24:24 +0300

    Change database provider from SQL Server to PostgreSQL

--- Program.cs
+++ Program.cs
@@ -6,8 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // DB
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
     )
 );
 
@@ -47,3 +46,4 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.Run();
+
